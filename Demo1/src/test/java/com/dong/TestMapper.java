8366abcc75bdbf7de5dedd8f3ff51f9b646a4358
苏无及 <dongf7889@gmail.com>
2022-08-01 11:28:37 +0800

    😁在MyBatis中加入log4j日志

--- Demo1/src/test/java/com/dong/TestMapper.java
+++ Demo1/src/test/java/com/dong/TestMapper.java
@@ -17,12 +17,16 @@ public class TestMapper {
         // 读取配置文件获得输入流
         InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
 
+        // 通过SqlSessionFactoryBuilder,传入输入流,获得SqlSessionFactory对象
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
 
+        // 获取SqlSession对象,传入true表示自动事务提交
         SqlSession sqlSession = sqlSessionFactory.openSession(true);
 
+        // 通过代理模式创建UserMapper的代理类对象
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
 
+        // 调用UserMapper接口中的方法,寻找相应的映射文件,执行SQL语句,返回结果集
         int result = userMapper.insertUser();
 
         System.out.println(result);
